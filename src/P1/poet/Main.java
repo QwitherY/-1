5b70d77c6596974720da46da8d10734d855fe054
QwitherY <117089257+QwitherY@users.noreply.github.com>
2024-05-31 20:10:41 +0800

    Update Main.java

--- src/P1/poet/Main.java
+++ src/P1/poet/Main.java
@@ -3,26 +3,11 @@
  */
 package P1.poet;
 
-import java.io.File;
-import java.io.IOException;
-
-/**
- * Example program using GraphPoet.
- * 
- * <p>PS2 instructions: you are free to change this example class.
- */
 public class Main {
     
     /**
      * Generate example poetry.
      * 
-     * @param args unused
-     * @throws IOException if a poet corpus file cannot be found or read
-     */
-    public static void main(String[] args) throws IOException {
-        final P1.poet.GraphPoet nimoy = new P1.poet.GraphPoet(new File("src/P1/poet/mugar-omni-theater.txt"));
-        final String input = "Test the system.";
-    
-    }
+     * @param args unus
     
 }
